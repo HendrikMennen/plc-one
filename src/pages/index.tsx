57624001873ca701ec56ab3eedfe8beeea8c06bf
Hendrik Mennen <hmennen@protop-solutions.com>
2023-04-16 23:17:14 +0200

    init

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,124 +1,109 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import { AiOutlineDownCircle } from 'react-icons/ai';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className='px-3 overflow-hidden'>
+      <div className='container m-auto text-white mb-24'>
+        <header className='h-48 w-full flex flex-column items-center'>
+          <div>
+            <Image src={"/PLC_ONE_L_F.png"} alt='PLC Logo' width={200} height={100} />
+          </div>
+        </header>
+
+        <div className='flex'>
+          <div className='flex flex-column items-center m-auto flex-wrap'>
+            <div style={{ marginRight: "-100px" }} className='text-4xl whitespace-nowrap md:mt-20'>
+              Get Ready for<br />
+              a <span className='font-bold'>new Chapter</span> in<br />
+              Industrial Automation<br />
+            </div>
+            <Image style={{ marginRight: "-100px" }} src={"/PLC_G.png"} alt='PLC Logo' width={600} height={200} />
+          </div>
         </div>
+
+        <div className='grid grid-cols-3 gap-3 text-center mt-20'>
+          <div>
+            <div className='font-bold text-3xl md:text-4xl'>5×</div>
+            <div className='mt-2 text-xl text-stone-400'>EFFICIENCY</div>
+          </div>
+          <div>
+            <div className='font-bold text-3xl md:text-4xl'>10×</div>
+            <div className='mt-2 text-xl text-stone-400'>CHEAPER</div>
+          </div>
+          <div>
+            <div className='font-bold text-3xl md:text-4xl'>1 MIO×</div>
+            <div className='mt-2 text-xl text-stone-400'>FASTER</div>
+          </div>
+        </div>
+
+        <AiOutlineDownCircle className='m-auto mt-28 h-10 w-10 text-stone-200' />
+
+        <div>
+          <Image className='m-auto' alt="PLC ONE CASE" src={"/Case.png"} width={1100} height={600} />
+        </div>
+
+        <div className='text-center mt-5 max-w-4xl m-auto mt-24'>
+          <h2 className='text-3xl font-bold mt-10'>One Simple Solution for the Tasks of the Future</h2>
+          <Image src={"/Compare.png"} className='mt-10' alt="Compare" height={500} width={1000} />
+          <div className='text-xl mt-12'>No special modules for just one purpose, because all data processing is done on one chip.
+            No unnessesary wireing and power supplies, because you only need one modular PCB</div>
+        </div>
+
+        <div className='text-center mt-5 max-w-4xl m-auto mt-24 lg:mt-32'>
+          <h2 className='text-3xl font-bold mt-10'>All You Need in One Modular System</h2>
+          <Image src={"/PLC_Example.png"} className='mt-10' alt="Compare" height={500} width={1000} />
+          <div className='text-xl mt-12'>All extensions are compliant with the CRUVI Standard. Use not just our modules,
+            but also extensions from industry leaders in the FPGA development space. Process data from a camera on the same
+            chip that runs your industrial plant.</div>
+        </div>
+
+        <div className='text-center mt-5 max-w-4xl m-auto mt-24 lg:mt-32'>
+          <h2 className='text-3xl font-bold mt-10'>Not just an Industry Controller</h2>
+          <Image src={"/PLC_System.png"} className='mt-10' alt="Compare" height={500} width={1000} />
+          <div className='text-xl mt-12'>Our flexible modular system can be used as an industry controler, but the processing
+            power also allows applications with neural networks and image processing. The ethernet extension then connects multiple systems.
+            The low price and small size also allows the direct integration in robots as PCB.</div>
+        </div>
+
+        <div className='text-center mt-5 max-w-4xl m-auto mt-24 lg:mt-32'>
+          <h2 className='text-3xl font-bold mt-10'>We Only Have One Earth</h2>
+          <div className='md:grid md:grid-cols-3 gap-3 text-center mt-10'>
+            <div>
+              <Image className='m-auto mt-10 md:mt-0' src={"/Minimalism.png"} alt="Minimalism" height={200} width={200} />
+              <h2 className='font-bold text-2xl'>Minimalism is More than you Need</h2>
+              <span>With only one chip and power supply, you need less electronics for more performance</span>
+            </div>
+            <div>
+              <Image className='m-auto mt-10 md:mt-0' src={"/Modular.png"} alt="Modular" height={200} width={200} />
+              <h2 className='font-bold text-2xl'>Fully Modular</h2>
+              <span>Only replace what is necessary with the modular PCBs</span>
+            </div>
+            <div>
+              <Image className='m-auto mt-10 md:mt-0' src={"/Energy.png"} alt="Energy" height={200} width={200} />
+              <h2 className='font-bold text-2xl'>Less Electronics for the Best Energy Efficiency</h2>
+              <span>Run your whole industrial plant
+                with the energy of just one small battery pack</span>
+            </div>
+          </div>
+        </div>
+
+        <div className='text-center mt-10 max-w-4xl m-auto mt-24 text-xl'>
+          <h2 className='text-3xl font-bold mt-10'>Contact</h2>
+          <div className='text-xl mt-5'>Beier & Wiegand Industrial Gbr</div>
+          <a href='mailto:info@plc-one.net'>info@plc-one.net</a>
+          <div>Hornscher Weg 23</div>
+          <div>32657 Lemgo</div>
+        </div>
+
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <footer className='container text-white text-center m-auto'>
+        <div>Copyright © 2022 PLC-ONE.net</div>
+      </footer>
     </main>
   )
 }
