e9d4f8821ac3ad335fc5d4c91a1d80a921adf105
Hendrik Mennen <hmennen@protop-solutions.com>
2023-04-18 18:43:04 +0200

    gerne

---
+++ src/pages/contact.tsx
@@ -0,0 +1,156 @@
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import { AiOutlineDownCircle } from 'react-icons/ai';
+import { RiPlantLine} from 'react-icons/ri';
+import { TfiWorld} from 'react-icons/tfi';
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Contact() {
+  return (
+    <main className='px-3 overflow-hidden'>
+        <title>PLC-ONE Contact</title>
+        <div className='container m-auto text-white mb-24'>
+        <header className='h-32 w-full flex flex-column'>
+          <a href='/'>
+            <Image src={"/PLC_ONE_L_F.png"} alt='PLC Logo' className="mt-10" width={260} height={150} />
+          </a>
+        </header>
+      <div className='m-auto mt-10 text-xl'>
+          <h2 className='text-3xl font-bold mt-10'>Contact</h2>
+          <div className='text-xl mt-5'>Beier & Wiegand Industrial Gbr</div>
+          <a href='mailto:info@plc-one.net'>info@plc-one.net</a>
+          <div>Hornscher Weg 23</div>
+          <div>32657 Lemgo</div>
+        </div>
+
+        <div className='mt-10 text-sm text-justify text-stone-100'>
+
+        <span className='text-xl'>Datenschutzerklärung</span> <br/>
+
+Die Nutzung unserer Webseite ist in der Regel ohne Angabe personenbezogener Daten möglich. Soweit auf unseren Seiten personenbezogene Daten (beispielsweise Name, Anschrift oder eMail-Adressen) erhoben werden, erfolgt dies, soweit möglich, stets auf freiwilliger Basis. Diese Daten werden ohne Ihre ausdrückliche Zustimmung nicht an Dritte weitergegeben.
+
+Wir weisen darauf hin, dass die Datenübertragung im Internet (z.B. bei der Kommunikation per E-Mail) Sicherheitslücken aufweisen kann. Ein lückenloser Schutz der Daten vor dem Zugriff durch Dritte ist nicht möglich.
+
+Der Nutzung von im Rahmen der Impressumspflicht veröffentlichten Kontaktdaten durch Dritte zur Übersendung von nicht ausdrücklich angeforderter Werbung und Informationsmaterialien wird hiermit ausdrücklich widersprochen. Die Betreiber der Seiten behalten sich ausdrücklich rechtliche Schritte im Falle der unverlangten Zusendung von Werbeinformationen, etwa durch Spam-Mails, vor.
+
+Ihre Betroffenenrechte
+Unter den angegebenen Kontaktdaten unseres Datenschutzbeauftragten können Sie jederzeit folgende Rechte ausüben:
+
+Auskunft über Ihre bei uns gespeicherten Daten und deren Verarbeitung (Art. 15 DSGVO),
+Berichtigung unrichtiger personenbezogener Daten (Art. 16 DSGVO),
+Löschung Ihrer bei uns gespeicherten Daten (Art. 17 DSGVO),
+Einschränkung der Datenverarbeitung, sofern wir Ihre Daten aufgrund gesetzlicher Pflichten noch nicht löschen dürfen (Art. 18 DSGVO),
+Widerspruch gegen die Verarbeitung Ihrer Daten bei uns (Art. 21 DSGVO) und
+Datenübertragbarkeit, sofern Sie in die Datenverarbeitung eingewilligt haben oder einen Vertrag mit uns abgeschlossen haben (Art. 20 DSGVO). Sofern Sie uns eine Einwilligung erteilt haben, können Sie diese jederzeit mit Wirkung für die Zukunft widerrufen.
+Sie können sich jederzeit mit einer Beschwerde an eine Aufsichtsbehörde wenden, z. B. an die zuständige Aufsichtsbehörde des Bundeslands Ihres Wohnsitzes oder an die für uns als verantwortliche Stelle zuständige Behörde.
+
+Eine Liste der Aufsichtsbehörden (für den nichtöffentlichen Bereich) mit Anschrift finden Sie unter: https://www.bfdi.bund.de/DE/Infothek/Anschriften_Links/anschriften_links-node.html.
+
+Eingebettete YouTube-Videos
+Art und Zweck der Verarbeitung:
+Auf einigen unserer Webseiten betten wir YouTube-Videos ein. Betreiber der entsprechenden Plugins ist die YouTube, LLC, 901 Cherry Ave., San Bruno, CA 94066, USA (nachfolgend „YouTube“). Wenn Sie eine Seite mit dem YouTube-Plugin besuchen, wird eine Verbindung zu Servern von YouTube hergestellt. Dabei wird YouTube mitgeteilt, welche Seiten Sie besuchen. Wenn Sie in Ihrem YouTube-Account eingeloggt sind, kann YouTube Ihr Surfverhalten Ihnen persönlich zuzuordnen. Dies verhindern Sie, indem Sie sich vorher aus Ihrem YouTube-Account ausloggen.
+
+Wird ein YouTube-Video gestartet, setzt der Anbieter Cookies ein, die Hinweise über das Nutzerverhalten sammeln.
+
+Weitere Informationen zu Zweck und Umfang der Datenerhebung und ihrer Verarbeitung durch YouTube erhalten Sie in den Datenschutzerklärungen des Anbieters, Dort erhalten Sie auch weitere Informationen zu Ihren diesbezüglichen Rechten und Einstellungsmöglichkeiten zum Schutze Ihrer Privatsphäre (https://policies.google.com/privacy). Google verarbeitet Ihre Daten in den USA und hat sich dem EU-US Privacy Shield unterworfen https://www.privacyshield.gov/EU-US-Framework
+
+Rechtsgrundlage:
+Rechtsgrundlage für die Einbindung von YouTube und dem damit verbundenen Datentransfer zu Google ist Ihre Einwilligung (Art. 6 Abs. 1 lit. a DSGVO).
+
+Empfänger:
+Der Aufruf von YouTube löst automatisch eine Verbindung zu Google aus.
+
+Speicherdauer und Widerruf der Einwilligung:
+Wer das Speichern von Cookies für das Google-Ad-Programm deaktiviert hat, wird auch beim Anschauen von YouTube-Videos mit keinen solchen Cookies rechnen müssen. YouTube legt aber auch in anderen Cookies nicht-personenbezogene Nutzungsinformationen ab. Möchten Sie dies verhindern, so müssen Sie das Speichern von Cookies im Browser blockieren.
+
+Weitere Informationen zum Datenschutz bei „YouTube“ finden Sie in der Datenschutzerklärung des Anbieters unter: https://www.google.de/intl/de/policies/privacy/
+
+Drittlandtransfer:
+Google verarbeitet Ihre Daten in den USA und hat sich dem EU_US Privacy Shield unterworfen https://www.privacyshield.gov/EU-US-Framework.
+
+Bereitstellung vorgeschrieben oder erforderlich:
+Die Bereitstellung Ihrer personenbezogenen Daten erfolgt freiwillig, allein auf Basis Ihrer Einwilligung. Sofern Sie den Zugriff unterbinden, kann es hierdurch zu Funktionseinschränkungen auf der Website kommen.
+
+SSL-Verschlüsselung
+Um die Sicherheit Ihrer Daten bei der Übertragung zu schützen, verwenden wir dem aktuellen Stand der Technik entsprechende Verschlüsselungsverfahren (z. B. SSL) über HTTPS.
+
+Änderung unserer Datenschutzbestimmungen
+Wir behalten uns vor, diese Datenschutzerklärung anzupassen, damit sie stets den aktuellen rechtlichen Anforderungen entspricht oder um Änderungen unserer Leistungen in der Datenschutzerklärung umzusetzen, z.B. bei der Einführung neuer Services. Für Ihren erneuten Besuch gilt dann die neue Datenschutzerklärung.
+
+Fragen an den Datenschutzbeauftragten
+Wenn Sie Fragen zum Datenschutz haben, schreiben Sie uns bitte eine E-Mail oder wenden Sie sich direkt an die für den Datenschutz verantwortliche Person in unserer Organisation:
+
+Leon Beier lbeier@protop-solutions.com Hornscher Weg 23 32657 Lemgo Deutschland
+
+Die Datenschutzerklärung wurde mithilfe der activeMind AG erstellt.
+
+</div>
+
+<div className='mt-10 text-sm text-justify text-stone-100'>
+
+        <span className='text-xl'>Privacy Policy</span> <br/>
+
+The use of our website is usually possible without providing personal data. If there is any personal data requested, it will not be disclosed to third parties without your explicit consent.
+
+Please note that data transmission over the Internet (for example, when communicating by e-mail) may have security vulnerabilities. A complete protection of the data from access by third parties is not possible.
+
+The use of contact information published by third parties for the purpose of sending unsolicited advertising and information materials is hereby expressly excluded. The operators of the pages expressly reserve the right to take legal action in the event of the unsolicited sending of advertising information, such as spam e-mails.
+
+Your Rights
+You can exercise the following rights at any time under the contact details of our data protection officer:
+
+Information about your stored data and their processing (Art. 15 GDPR),
+correction of incorrect personal data (Art. 16 GDPR),
+deletion of your stored data (Art. 17 GDPR),
+limitation of data processing, if we are not yet allowed to delete your data due to legal obligations (Art. 18 GDPR),
+Objection against the processing of your data with us (Article 21 GDPR) and
+Data portability, provided you have consented to the data processing or have concluded a contract with us (Article 20 GDPR). If you have given us your consent, you can revoke it at any time with effect for the future.
+You can contact a regulatory authority at any time with a complaint, such as the competent supervisory authority of the federal state of your domicile or to the authority responsible for us as responsible authority.
+
+Embedded YouTube videos
+Nature and purpose of processing:
+On some of our websites, we embed YouTube videos. The respective plug-ins are operated by YouTube, LLC, 901 Cherry Ave., San Bruno, CA 94066, USA (hereafter "YouTube"). When you visit a page with the YouTube plug-in, it will connect to YouTube's servers. It tells YouTube which pages you visit. If you are logged in to your YouTube account, YouTube may personally associate your browsing behavior with you. This can be prevented by logging out of your YouTube account beforehand.
+
+If a YouTube video is started, the provider uses cookies that collect information about user behavior.
+
+For more information on the purpose and scope of your data collection and processing through YouTube, please refer to the provider's privacy policy, where you can also find out more about your rights and preferences for privacy (https://policies.google.com/privacy). , Google processes your data in the US and has submitted to the EU-US Privacy Shield https://www.privacyshield.gov/EU-US Framework
+
+Legal basis:
+The legal basis for the integration of YouTube and the associated data transfer to Google is your consent (Article 6 (1) (a) GDPR).
+
+Receiver:
+The YouTube invocation automatically triggers a connection to Google.
+
+Storage period and revocation of consent:
+Anyone who has disabled the storage of cookies for the Google Ad program will not have to expect any such cookies when watching YouTube videos. However, YouTube also stores non-personal usage information in other cookies. If you want to prevent this, you must block the storage of cookies in the browser.
+
+Further information on data protection at "YouTube" can be found in the provider's privacy policy at: https://www.google.com/intl/en/policies/privacy/
+
+Third country transfer:
+Google processes your data in the US and has submitted to the EU_US Privacy Shield https://www.privacyshield.gov/EU-US-Framework
+
+Provision required:
+The provision of your personal data is voluntary, solely on the basis of your consent. If you prevent access, this may result in functional restrictions on the website.
+
+SSL encryption
+To protect the security of your data during transmission, we use state-of-the-art encryption techniques (such as SSL) over HTTPS.
+
+Change to our privacy policy
+We reserve the right to amend this privacy policy to always comply with the current legal requirements or to implement changes to our services in the privacy policy, e.g. when introducing new services. Your new visit will be subject to the new privacy policy.
+
+Questions to the Data Protection Officer
+If you have any questions about privacy, please email us or contact the person responsible for data protection in our organization:
+
+Leon Beier lbeier@protop-solutions.com Hornscher Weg 23 32657 Lemgo Germany
+
+The privacy policy was created with the help of activeMind AG.
+</div>
+        </div>
+
+
+        <div>
+            
+        </div>
+    </main>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,58 +1,60 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import { AiOutlineDownCircle } from 'react-icons/ai';
-
+import { RiPlantLine } from 'react-icons/ri';
+import { TfiWorld } from 'react-icons/tfi';
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <main className='px-3 overflow-hidden'>
+      <title>PLC-ONE</title>
       <div className='container m-auto text-white mb-24'>
-        <header className='h-48 w-full flex flex-column items-center'>
+        <header className='h-32 w-full flex flex-column absolute xl:ml-16'>
           <div>
-            <Image src={"/PLC_ONE_L_F.png"} alt='PLC Logo' width={200} height={100} />
+            <Image src={"/PLC_ONE_L_F.png"} alt='PLC Logo' className="mt-10" width={260} height={150} />
           </div>
         </header>
 
         <div className='flex'>
-          <div className='flex flex-column items-center m-auto flex-wrap'>
-            <div style={{ marginRight: "-100px" }} className='text-5xl whitespace-nowrap md:mt-20'>
+          <div className='flex flex-column items-center m-auto flex-wrap mt-32 md:mt-10'>
+            <div style={{ marginRight: "-94px" }} className='text-4xl md:text-5xl whitespace-nowrap md:mt-20'>
               Get Ready for<br />
-              a <span className='font-bold'>new Chapter</span> in<br />
+              a <span className='font-bold'>New Chapter</span> in<br />
               Industrial Automation<br />
             </div>
-            <Image style={{ marginRight: "-100px" }} src={"/PLC_G.png"} alt='PLC Logo' width={800} height={200} />
+            <Image style={{ marginRight: "-100px" }} src={"/PLC_G.png"} alt='PLC Logo' width={800} height={500} />
           </div>
         </div>
 
         <div className='grid grid-cols-3 gap-3 text-center mt-20'>
           <div>
-            <div className='font-bold text-3xl md:text-4xl'>5×</div>
-            <div className='mt-2 text-xl text-stone-400'>EFFICIENCY</div>
+            <div className='font-bold text-3xl md:text-6xl'>5×</div>
+            <div className='mt-2 text-xl text-stone-400 md:text-3xl'>EFFICIENCY*</div>
           </div>
           <div>
-            <div className='font-bold text-3xl md:text-4xl'>10×</div>
-            <div className='mt-2 text-xl text-stone-400'>CHEAPER</div>
+            <div className='font-bold text-3xl md:text-6xl'>10×</div>
+            <div className='mt-2 text-xl text-stone-400 md:text-3xl'>VALUE*</div>
           </div>
           <div>
-            <div className='font-bold text-3xl md:text-4xl'>1 MIO×</div>
-            <div className='mt-2 text-xl text-stone-400'>FASTER</div>
+            <div className='font-bold text-3xl md:text-6xl'>1 MIO×</div>
+            <div className='mt-2 text-xl text-stone-400 md:text-3xl'>FASTER*</div>
           </div>
         </div>
 
         <div>
-          <Image className='m-auto' alt="PLC ONE CASE" src={"/Case.png"} width={1100} height={600} />
+          <Image className='m-auto mt-10' alt="PLC ONE CASE" src={"/Case.png"} width={1100} height={600} />
         </div>
 
         <div className='text-center mt-5 max-w-4xl m-auto mt-24'>
-          <h2 className='text-3xl font-bold mt-10'>One Simple Solution for the Tasks of the Future</h2>
+          <h2 className='text-4xl font-bold mt-10'>One Simple Solution for the Tasks of the Future</h2>
           <Image src={"/Compare.png"} className='mt-10' alt="Compare" height={500} width={1000} />
           <div className='text-xl mt-12'>No special modules for just one purpose, because all data processing is done on one chip.
-            No unnessesary wireing and power supplies, because you only need one modular PCB</div>
+            No unnecessary wiring and power supplies, only one modular PCB is needed</div>
         </div>
 
         <div className='text-center mt-5 max-w-4xl m-auto mt-24 lg:mt-32'>
-          <h2 className='text-3xl font-bold mt-10'>All You Need in One Modular System</h2>
+          <h2 className='text-4xl font-bold mt-10'>All You Need in One Modular System</h2>
           <Image src={"/PLC_Example.png"} className='mt-10' alt="Compare" height={500} width={1000} />
           <div className='text-xl mt-12'>All extensions are compliant with the CRUVI Standard. Use not just our modules,
             but also extensions from industry leaders in the FPGA development space. Process data from a camera on the same
@@ -60,47 +62,87 @@ export default function Home() {
         </div>
 
         <div className='text-center mt-5 max-w-4xl m-auto mt-24 lg:mt-32'>
-          <h2 className='text-3xl font-bold mt-10'>Not just an Industry Controller</h2>
+          <h2 className='text-4xl font-bold mt-10'>Not an ordinary Industrial Controller</h2>
           <Image src={"/PLC_System.png"} className='mt-10' alt="Compare" height={500} width={1000} />
-          <div className='text-xl mt-12'>Our flexible modular system can be used as an industry controler, but the processing
-            power also allows applications with neural networks and image processing. The ethernet extension then connects multiple systems.
-            The low price and small size also allows the direct integration in robots as PCB.</div>
+          <div className='text-xl mt-12'>Our modular system can be used as an industrial controller, the processing power also allows applications with neural networks and image processing.
+            The ethernet extension then connects multiple systems. Low price and small form factor also allows direct integration in robotics as PCB.</div>
         </div>
 
         <div className='text-center mt-5 max-w-4xl m-auto mt-24 lg:mt-32'>
-          <h2 className='text-3xl font-bold mt-10'>We Only Have One Earth</h2>
+          <h2 className='text-4xl font-bold mt-10'>We Only Have One Earth</h2>
           <div className='md:grid md:grid-cols-3 gap-3 text-center mt-10'>
             <div>
-              <Image className='m-auto mt-10 md:mt-0' src={"/Minimalism.png"} alt="Minimalism" height={160} width={160} />
-              <h2 className='font-bold text-2xl'>Minimalism is More than you Need</h2>
-              <span>With only one chip and power supply, you need less electronics for more performance</span>
+              <TfiWorld style={{ height: 140, width: 140, color: "#0092db" }} className='text-center m-auto p-3' />
+              <h2 className='font-bold text-2xl'>Minimalism</h2>
+              <span>One chip and power supply, less electronics, more performance</span>
             </div>
             <div>
-              <Image className='m-auto mt-10 md:mt-0' src={"/Modular.png"} alt="Modular" height={160} width={160} />
+              <Image className='m-auto mt-10 md:mt-0' src={"/Modular.png"} alt="Modular" height={140} width={140} />
               <h2 className='font-bold text-2xl'>Fully Modular</h2>
-              <span>Only replace what is necessary with the modular PCBs</span>
+              <span>Only replace what is necessary</span>
             </div>
             <div>
-              <Image className='m-auto mt-10 md:mt-0' src={"/Energy.png"} alt="Energy" height={160} width={160} />
-              <h2 className='font-bold text-2xl'>Less Electronics for the Best Energy Efficiency</h2>
-              <span>Run your whole industrial plant
-                with the energy of just one small battery pack</span>
+              <RiPlantLine style={{ height: 140, width: 140, color: "#1ED760" }} className='text-center m-auto mt-10 md:mt-0' />
+              <h2 className='font-bold text-2xl'>Best Energy Efficiency</h2>
+              <span>Run data processing for a whole industrial plant of just one small battery pack</span>
             </div>
           </div>
         </div>
 
-        <div className='text-center mt-10 max-w-4xl m-auto mt-24 text-xl'>
-          <h2 className='text-3xl font-bold mt-10'>Contact</h2>
-          <div className='text-xl mt-5'>Beier & Wiegand Industrial Gbr</div>
-          <a href='mailto:info@plc-one.net'>info@plc-one.net</a>
-          <div>Hornscher Weg 23</div>
-          <div>32657 Lemgo</div>
+
+        <div className='text-center mt-5 max-w-2xl m-auto mt-24 lg:mt-32'>
+          <h2 className='text-4xl font-bold mt-10'>Our Team</h2>
+          <div className='md:grid md:grid-cols-2 text-center mt-10'>
+            <div>
+              <div className='m-auto md:mt-0' >
+                <Image className='m-auto md:mt-0' src={"/Leon.png"} alt="Modular" height={170} width={170} />
+              </div>
+              <div className='text-xl mt-2'>Leon Beier</div>
+              <div>
+              <a className='text-stone-500 hover:text-stone-100' href='https://www.linkedin.com/in/leon-beier-55bbb7230/' target='_blank'>LinkedIn</a>
+              </div>
+              <div>
+              <a className='text-stone-500 hover:text-stone-100' href='mailto:lbeier@protop-solutions.com'>lbeier@plc-one.net</a>
+              </div>
+            </div>
+            <div>
+              <div className='m-auto md:mt-0' >
+                <Image className='m-auto md:mt-0' src={"/Leo.png"} alt="Modular" height={170} width={170} />
+              </div>
+              <div className='text-xl mt-2'>Leo Wiegand</div>
+              <div>
+              <a className='text-stone-500 hover:text-stone-100' href='https://www.linkedin.com/in/leo-wiegand-b27aa0272/' target='_blank'>LinkedIn</a>
+              </div>
+              <div>
+              <a className='text-stone-500 hover:text-stone-100' href='mailto:lbeier@protop-solutions.com'>lwiegand@plc-one.net</a>
+              </div>
+            </div>
+
+          </div>
         </div>
+
+        <div className='text-center mt-5 max-w-2xl m-auto mt-24 lg:mt-32'>
+          <h2 className='text-4xl font-bold mt-10'>The Story Behind our Idea</h2>
+          <div className='mt-10 text-lg' >
+            Our experience with other PLCs led to PLC One. We were tired of old IDEs and complex PLCs that cost a fortune.
+            We thought about how we can make PLCs as simple as possible and created a revolutionary modular and environmentally friendly Controller.
+            Our focus was to deliver the easiest solution starting from the purchase process all the way to installation and maintenance.
+          </div>
+        </div>
+
 
       </div>
 
       <footer className='container text-white text-center m-auto'>
-        <div>Copyright © 2022 PLC-ONE.net</div>
+
+
+        <div className='mt-5 text-center text-sm'>*Compared to a system that uses a Cyclone V FPGA with embedded ARM processor and RTOS which is similar to common PLCs,
+          running a typical program  with a cycle time of 100 ms. The FPGA in the PLC ONE needs less than 100 ns to process the data. Price includes only production costs of named examples.</div>
+
+        <div className='mt-5 text-stone-500 hover:text-stone-100'>
+          <a href="/contact" >Contact / Impressum</a>
+        </div>
+        <div className='my-5'>Copyright © 2022 PLC-ONE.net</div>
       </footer>
     </main>
   )
